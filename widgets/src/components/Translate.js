1358adc53afe149ec2feff0010d8394f169729e4
superterminal <trendemil@gmail.com>
2020-12-21 08:31:19 +0200

    Add Translate component

---
+++ widgets/src/components/Translate.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import Dropdown from './Dropdown';
+
+// AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM
+
+const options = [
+    {
+        label: 'Afrikaans',
+        value: 'af'
+    }, 
+    {
+        label: 'Arabic',
+        value: 'ar'
+    },
+    {
+        label: 'Hindi',
+        value: 'hi'
+    }
+]
+
+const Translate = () => {
+
+    const [language, setLanguage] = useState(options[0]);
+    const [text, setText] = useState('');
+
+    return (
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Text</label>
+                    <input value={text} onChange={(e) => setText(e.target.value)} />
+                </div>
+            </div>
+            <Dropdown 
+                label="Select a language"
+                selected={language}
+                options={options}
+                onSelectedChange={setLanguage} 
+            />
+        </div>
+    );
+};
+
+export default Translate;
