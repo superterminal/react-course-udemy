43e48d8862142a53aba914a2f290a0672cb78150
superterminal <trendemil@gmail.com>
2021-01-05 22:47:52 +0200

    Rework classes to functional components

--- videos-project/src/components/App.js
+++ videos-project/src/components/App.js
@@ -1,54 +1,99 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
 
-class App extends React.Component {
+const App = () => {
 
-    state = { videos: [], selectedVideo: null };
+    const [video, setVideo] = useState([]);
+    const [selectedVideo, setSelectedVideo] = useState(null);
 
-    componentDidMount() {
-        this.onTermSubmit('buildings');
-    }
+    useEffect(() => {
+        onTermSubmit('buildings');
+    }, []);
 
-    onTermSubmit = async term => {
+    const onTermSubmit = async term => {
         const response = await youtube.get('/search', {
             params: {
                 q: term
             }
         });
 
-        this.setState({ 
-            videos: response.data.items,
-            selectedVideo: response.data.items[0] 
-        });
+        setVideo(response.data.items);
+        setSelectedVideo(response.data.items[0]);
     };
 
-    onVideoSelect = video => {
-      this.setState({ selectedVideo: video });
-    };
+     const onVideoSelect = video => {
+        setSelectedVideo(video);
+      };
 
-    render() {
-        return (
-            <div className="ui container">
-                <SearchBar onFormSubmit={this.onTermSubmit}/>
-                <div className="ui grid">
-                    <div className="ui row">
-                        <div className="eleven wide column">
-                            <VideoDetail video={this.state.selectedVideo} />
-                        </div>
-                        <div className="five wide column">
-                            <VideoList 
-                                onVideoSelect={this.onVideoSelect} 
-                                videos={this.state.videos}
-                            />    
-                        </div>
+      return (
+        <div className="ui container">
+            <SearchBar onFormSubmit={onTermSubmit}/>
+            <div className="ui grid">
+                <div className="ui row">
+                    <div className="eleven wide column">
+                        <VideoDetail video={selectedVideo} />
+                    </div>
+                    <div className="five wide column">
+                        <VideoList 
+                            onVideoSelect={onVideoSelect} 
+                            videos={video}
+                        />    
                     </div>
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+
+};
+
+// class App extends React.Component {
+
+    // state = { videos: [], selectedVideo: null };
+
+    // componentDidMount() {
+    //     this.onTermSubmit('buildings');
+    // }
+
+    // onTermSubmit = async term => {
+    //     const response = await youtube.get('/search', {
+    //         params: {
+    //             q: term
+    //         }
+    //     });
+
+    //     this.setState({ 
+    //         videos: response.data.items,
+    //         selectedVideo: response.data.items[0] 
+    //     });
+    // };
+
+    // onVideoSelect = video => {
+    //   this.setState({ selectedVideo: video });
+    // };
+
+//     render() {
+//         return (
+//             <div className="ui container">
+//                 <SearchBar onFormSubmit={this.onTermSubmit}/>
+//                 <div className="ui grid">
+//                     <div className="ui row">
+//                         <div className="eleven wide column">
+//                             <VideoDetail video={this.state.selectedVideo} />
+//                         </div>
+//                         <div className="five wide column">
+//                             <VideoList 
+//                                 onVideoSelect={this.onVideoSelect} 
+//                                 videos={this.state.videos}
+//                             />    
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
 
 export default App;
